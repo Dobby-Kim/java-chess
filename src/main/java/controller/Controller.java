b8e524ec12f4de50151bad71d75562a50eadd6a2
DobbyMac <kimdobby@g.skku.edu>
2024-04-01 14:39:08 +0900

    feat(Controller): 현재 점수 입출력 요구 기능 구현

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,9 +6,11 @@ import static view.OutputView.printInitialGamePrompt;
 
 import java.util.List;
 import model.chessboard.ChessBoard;
+import model.chessboard.Score;
 import model.position.Position;
 import view.GameCommand;
 import view.InputView;
+import view.OutputView;
 import view.dto.InfoMapper;
 import view.dto.PieceInfo;
 
@@ -17,13 +19,23 @@ public class Controller {
     public void execute() {
         GameCommand gameCommand = executeInitial();
         ChessBoard chessBoard = new ChessBoard();
-        while (gameCommand != GameCommand.END && !chessBoard.isFinish()) {
+        while (!gameCommand.isEnd() && !chessBoard.isFinish()) {
             List<PieceInfo> pieceInfos = InfoMapper.toPieceInfoMapper(chessBoard);
             printChessBoard(pieceInfos);
             gameCommand = inputRetryHelper(() -> runGame(chessBoard));
+            showCurrentStatus(gameCommand, chessBoard);
         }
         List<PieceInfo> pieceInfos = InfoMapper.toPieceInfoMapper(chessBoard);
         printChessBoard(pieceInfos);
+        Score currentScore = chessBoard.aggregateScore();
+        OutputView.printScore(currentScore);
+    }
+
+    private void showCurrentStatus(GameCommand gameCommand, ChessBoard chessBoard) {
+        if (gameCommand == GameCommand.STATUS) {
+            Score currentScore = chessBoard.aggregateScore();
+            OutputView.printScore(currentScore);
+        }
     }
 
     private GameCommand executeInitial() {
